12e051f358ccb763c19e3860237921e7dff62e28
Joshua Kibwage <joshuakibwage23@gmail.com>
2025-12-08 11:11:31 +0300

    feat: implement dashboard page

--- admin/src/lib/axios.js
+++ admin/src/lib/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-if (!import.meta.env.VITE_API_URL) {
-  throw new Error('VITE_API_URL environment variable is required');
-}
+// if (!import.meta.env.VITE_API_URL) {
+//   throw new Error('VITE_API_URL environment variable is required');
+// }
 
 const axiosInstance = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
