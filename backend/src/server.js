702b79cf215bfacc031e79abe43b347549aa7b42
Joshua Kibwage <joshuakibwage23@gmail.com>
2025-12-09 14:14:22 +0300

    tiny fixes

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,8 @@
 import express from "express";
 import path from "path";
-import { clerkMiddleware } from '@clerk/express';
+import { clerkMiddleware } from "@clerk/express";
 import { serve } from "inngest/express";
+import cors from "cors";
 
 import { functions, inngest } from "./config/inngest.js";
 
@@ -15,17 +16,15 @@ import reviewRoutes from "./routes/review.route.js";
 import productRoutes from "./routes/product.route.js";
 import cartRoutes from "./routes/cart.route.js";
 
-
-
 const app = express();
 
 const __dirname = path.resolve();
 
 app.use(express.json());
+app.use(clerkMiddleware()); // adds auth object under the req => req.auth
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true })); // credentials: true allows the browser to send the cookies to the server with the request
 
-app.use(clerkMiddleware())  //adds auth object under the req
-
-app.use("/api/inngest", serve({client:inngest, functions}));
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 app.use("/api/admin", adminRoutes);
 app.use("/api/users", userRoutes);
@@ -34,25 +33,24 @@ app.use("/api/reviews", reviewRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
 
-
 app.get("/api/health", (req, res) => {
-    res.status(200).json({message: "success!"});
+  res.status(200).json({ message: "Success" });
 });
 
-//make our app ready for deployment
-if(ENV.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../admin/dist")));
+// make our app ready for deployment
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../admin/dist")));
 
-    app.get("/{*any}", (req, res) => {
-        res.sendFile(path.join(__dirname, "../admin", "dist", "index.html"))
-    })
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../admin", "dist", "index.html"));
+  });
 }
 
-const startServer = async() => {
-    await connectDB();
-    app.listen(ENV.PORT, () => {
-        console.log("Server is up and running");
-    });
+const startServer = async () => {
+  await connectDB();
+  app.listen(ENV.PORT, () => {
+    console.log("Server is up and running");
+  });
 };
 
 startServer();
